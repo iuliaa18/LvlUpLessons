1cee38fca5e3a4c7d2a0b84069825b4f095c7d2b
iuliaa18 <pahmutova1806@gmail.com>
2022-10-05 22:19:10 +0300

    class 2 30/09 TASKS

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 a = int(input("Введите число:"))
 b = int(input("Введите число:"))
-if a < b:
-    print(a)
-elif a > b:
-    print(b)
+c = int(input("Введите число:"))
+if a > b > c:
+    print(c, b, a)
+elif a > c > b:
+    print(b, c, a)
+elif b > c > a:
+    print(a, c, b)
+elif c > a > b:
+    print(b, a, c)
 else:
-    print("Все равно")
+    print(a, b, c)
+
